3815289a861ebf210db1e04fdf7447b332dd0bc1
Keith Wong <jianhua@jianhua.me>
2021-10-14 01:59:07 -0400

    Android gradle build

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit, OnDestroy {
+  result = null;
+  scanActive = false;
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  ngAfterViewInit() {
+    BarcodeScanner.prepare();
+  }
+
+  ngOnDestroy() {
+    BarcodeScanner.stopScan();
+  }
+
+  async startScanner() {
+    const allowed = await this.checkPermission();
+    if (allowed) {
+      this.scanActive = true;
+      const result = await BarcodeScanner.startScan();
+      console.log(
+        '🚀 ~ file: home.page.ts ~ line 25 ~ HomePage ~ startScanner ~ result',
+        result
+      );
+      if (result.hasContent) {
+        this.result = result.content;
+        this.scanActive = false;
+      }
+    }
+  }
+
+  async checkPermission() {
+    return new Promise(async (resolve, reject) => {
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        resolve(true);
+      } else if (status.denied) {
+        const alert = await this.alertController.create({
+          header: 'No permission',
+          message: 'Please allow camera access in your settings',
+          buttons: [
+            {
+              text: 'No',
+              role: 'cancel',
+            },
+            {
+              text: 'Open Settings',
+              handler: () => {
+                BarcodeScanner.openAppSettings();
+                resolve(false);
+              },
+            },
+          ],
+        });
+        await alert.present();
+      } else {
+        resolve(false);
+      }
+    });
+  }
+
+  stopScanner() {
+    BarcodeScanner.startScan();
+    this.scanActive = false;
+  }
 }
